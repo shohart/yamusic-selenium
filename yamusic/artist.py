4af0c5ca861eaa864dd0614c044492bb9f14f357
Shohart <s.sovetnik@gmail.com>
2023-08-22 20:48:57 +0300

    selenium changes fix

--- yamusic/artist.py
+++ yamusic/artist.py
@@ -1,9 +1,17 @@
-from .misc import Idable, Findable, find_or_new, \
-                  LazyClass, lazyproperty, \
-                  find_elements_in_scrollpane, seleniumdriven
+from .misc import (
+    Idable,
+    Findable,
+    find_or_new,
+    LazyClass,
+    lazyproperty,
+    find_elements_in_scrollpane,
+    seleniumdriven,
+)
+
 
 def find(_id):
     return find_or_new(Artist, _id)
+
 
 class Artist(Idable, Findable, LazyClass):
     BASE = "https://music.yandex.ru/artist/{id}"
@@ -17,50 +25,70 @@ class Artist(Idable, Findable, LazyClass):
     @lazyproperty
     @seleniumdriven()
     def title(self, driver):
-        return driver.find_element_by_class_name('page-artist__title').text
+        return driver.find_element_by_class_name("page-artist__title").text
 
     @lazyproperty
     @seleniumdriven("/albums")
     # TODO: Handle multiple artists
     def albums(self, driver):
         def process(el):
-            _id = el.find_element_by_tag_name('a').get_attribute('href').split('/')[-1]
-            title = el.find_element_by_class_name('album__title').text
-            year = el.find_element_by_class_name('album__year').text
-            year = year.split(' ')[0] if year is not None and len(year) > 0 else None
+            _id = el.find_element_by_tag_name("a").get_attribute("href").split("/")[-1]
+            title = el.find_element_by_class_name("album__title").text
+            year = el.find_element_by_class_name("album__year").text
+            year = year.split(" ")[0] if year is not None and len(year) > 0 else None
             album = Album.find(_id)
             album.title = title
             album.year = year
             album.artist = self
             return album
-        return find_elements_in_scrollpane(driver, lambda: driver.find_elements_by_class_name('album'), process)
+
+        return find_elements_in_scrollpane(
+            driver, lambda: driver.find_elements_by_class_name("album"), process
+        )
 
     @lazyproperty
     @seleniumdriven("/tracks")
     def songs(self, driver):
         def process(el):
             def process_album(el):
-                _id = el.find_element_by_tag_name('a').get_attribute('href').split('/')[-1]
+                _id = (
+                    el.find_element("tag_name", "a")
+                    .get_attribute("href")
+                    .split("/")[-1]
+                )
                 title = el.text
                 album = Album.find(_id)
                 album.title = title
                 album.artist = self
                 return album
+
             def process_trackname(el, album):
-                _id = el.find_element_by_tag_name('a').get_attribute('href').split('/')[-1]
+                _id = (
+                    el.find_element("tag_name", "a")
+                    .get_attribute("href")
+                    .split("/")[-1]
+                )
                 title = el.text
                 song = Song.find(album.id, _id)
                 song.title = title
                 return song
+
             def process_trackinfo(el):
                 song.duration = el.text
                 return song
-            album = process_album(el.find_element_by_class_name('d-track__meta'))
-            song = process_trackname(el.find_element_by_class_name('d-track__name'), album)
-            song = process_trackinfo(el.find_element_by_class_name('typo-track'))
+
+            album = process_album(el.find_element_by_class_name("d-track__meta"))
+            song = process_trackname(
+                el.find_element_by_class_name("d-track__name"), album
+            )
+            song = process_trackinfo(el.find_element_by_class_name("typo-track"))
             song.album = album
             return song
-        return find_elements_in_scrollpane(driver, lambda: driver.find_elements_by_class_name('d-track'), process)
+
+        return find_elements_in_scrollpane(
+            driver, lambda: driver.find_elements_by_class_name("d-track"), process
+        )
+
 
 from .album import Album
 from .song import Song
